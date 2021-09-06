dcec81fb8c685087d1b68c0c837563c7fe0650be
Ian F <ian.fiddeman@gmail.com>
2021-09-06 09:55:19 +0100

    async await added

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  function fetchMoviesHandler(){
-    fetch('https://swapi.dev/api/films').then(response => {
-      return response.json();
-    }).then(data => {
-      const transformedMovies = data.results.map( movieData => {
+
+  async function fetchMoviesHandler(){
+    const response = await fetch('https://swapi.dev/api/films')
+    const data = await response.json();
+  
+    const transformedMovies = data.results.map( movieData => {
         return {
           id: movieData.episode_id,
           title: movieData.title,
@@ -19,7 +20,7 @@ function App() {
         }
       })
       setMovies(transformedMovies);
-    });;
+    ;;
   }
 
   return (
@@ -32,6 +33,6 @@ function App() {
       </section>
     </React.Fragment>
   );
-}
+  }
 
 export default App;
